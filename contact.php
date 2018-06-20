4e59287ced9a64c26130923c27017cacd345616d
Gabriel Corso <gabe.17@dartmouth.edu>
2018-06-20 14:53:59 -0400

    initial

---
+++ contact.php
@@ -0,0 +1,368 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html dir="ltr" lang="en-US" class="ie6"> <![endif]-->
+<!--[if IE 7]>    <html dir="ltr" lang="en-US" class="ie7"> <![endif]-->
+<!--[if IE 8]>    <html dir="ltr" lang="en-US" class="ie8"> <![endif]-->
+<!--[if gt IE 8]><!--> <html dir="ltr" lang="en-US"> <!--<![endif]-->
+
+<!-- BEGIN head -->
+<head>
+
+	<!--Meta Tags-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="description" content="Wallace Yacht Services was founded in 1986 to provide high quality marine services to boat owners in Southwest Florida, ensuring a sense of confidence and comfort.">
+	
+	<!-- Title -->
+	<title>Wallace Yacht Services</title>
+	
+	<!-- JavaScript (must go in header) -->
+	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+	<script type="text/javascript" src="js/fontawesome-markers.min.js"></script>
+	
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/style.css" type="text/css"  media="all" />
+	<link rel="stylesheet" href="css/color-blue.css" type="text/css"  media="all" />
+	<link rel="stylesheet" href="css/responsive.css" type="text/css"  media="all" />
+	<link href="css/font-awesome.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="rs-plugin/css/settings.css">
+	<link rel="stylesheet" type="text/css" href="rs-plugin/css/layers.css">
+	<link rel="stylesheet" type="text/css" href="rs-plugin/css/navigation.css">
+	<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+	<link rel="stylesheet" type="text/css" href="css/prettyPhoto.css">
+	<link href='//fonts.googleapis.com/css?family=Source+Sans+Pro:400,200,200italic,300,300italic,400italic,600,600italic,700,700italic,900,900italic|Source+Serif+Pro:400,600,700' rel='stylesheet' type='text/css'>
+	
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+<!-- Global Site Tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-107577745-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-107577745-1');
+</script>
+<!-- END head -->
+</head>
+
+<!-- BEGIN body -->
+<body>
+	
+	<!-- BEGIN .outer-wrapper -->
+	<div class="outer-wrapper">
+	
+		<!-- BEGIN .top-bar-wrapper -->
+		<div class="top-bar-wrapper">
+
+			<!-- BEGIN .top-bar -->
+			<div class="top-bar clearfix">
+
+				<div class="top-bar-left clearfix">
+					<ul>
+						<li class="top-list-phone">(239) 776-2288</li>
+						<li class="top-list-business-hours">Mon - Sun 8:00 am - 6:00 pm</li>
+					</ul>
+				</div>
+
+				<div class="top-bar-right clearfix">
+					<ul>
+						<li class="top-list-facebook"><a href="https://www.facebook.com/pages/Wallace-Yacht-Service/964976123542991">Facebook</a></li>
+						<li><i class="fa fa-instagram"></i><a href="https://www.instagram.com/wallaceyachtservices/">&nbsp;&nbsp;Instagram</a></li>	
+					</ul>
+				</div>
+
+			<!-- END .top-bar -->
+			</div>
+
+		<!-- END .top-bar-wrapper -->
+		</div>
+
+		<!-- BEGIN .header-wrapper -->
+		<header class="header-wrapper clearfix">
+
+			<!-- BEGIN .header-inner -->
+			<div class="header-inner">
+
+				<div class="logo"><a href="index.html"><img src="Wallace Yacht pics/logo.png" alt="" height="80" width="85"></a>
+					<a href="index.html"><font size="5">Wallace Yacht Services</font></a>
+				</div>
+
+				<a href="contact.php" class="top-right-button"><i class="fa fa-arrow-circle-right"></i>Contact Us Now</a>
+
+				<!-- BEGIN #primary-navigation -->
+				<nav id="primary-navigation" class="navigation-wrapper fixed-navigation clearfix">
+
+					<!-- BEGIN .navigation-inner -->
+					<div class="navigation-inner">
+
+						<!-- BEGIN .navigation -->
+						<div class="navigation">
+							<ul>	
+								<li class="current-menu-item current_page_item"><a href="index.html">Home</a></li>
+								<li>
+									<a href="about.html">About Us</a>
+										</li>
+								<li>
+									<a href="photos.html">Photos</a></li>
+								<li><a href="contact.php">Contact</a>
+								</li>
+							</ul>
+						<!-- END .navigation -->
+						</div>
+
+						<a href="contact.php" class="top-right-button"><i class="fa fa-arrow-circle-right"></i>Contact Us</a>
+
+					<!-- END .navigation-inner -->
+					</div>
+
+				<!-- END #primary-navigation -->
+				</nav>
+
+				<div id="mobile-navigation">
+					<a href="#" id="mobile-navigation-btn"><i class="fa fa-bars"></i></a>
+				</div>
+
+				<div class="clearboth"></div>
+
+				<!-- BEGIN .mobile-navigation-wrapper -->
+				<div class="mobile-navigation-wrapper">	
+							<ul>	
+								<li class="current-menu-item current_page_item"><a href="index.html">Home</a></li>
+								<li>
+									<a href="about.html">About Us</a>
+										</li>
+								<li>
+									<a href="photos.html">Photos</a></li>
+								<li><a href="contact.php">Contact</a>
+								</li>
+							</ul>
+				<!-- END .mobile-navigation-wrapper -->
+				</div>
+
+			<!-- END .header-inner -->
+			</div>
+
+		<!-- END .header-wrapper -->
+		</header>
+
+		<div id="page-header">
+			<h1>Contact Us</h1>
+			<div class="title-block3"></div>
+			<p><a href="index.html">Home</a><i class="fa fa-angle-right"></i>Contact Us</p>
+		</div>
+		
+		<!-- BEGIN .content-wrapper -->
+		<div class="content-wrapper clearfix">
+			
+			<!-- BEGIN .main-content -->
+			<section class="main-content main-content-full">
+				
+				<div class="qns-one-half">
+					
+					<h4>Get In Touch</h4>
+					<div class="title-block5"></div>
+					<p>Wallace Yacht Services was founded in 1986 to provide high quality marine services to boat owners in Southwest Florida, ensuring a sense of confidence and comfort. The company is based in Naples, Florida and strives to protect and maintain its clients' investments.</p><p>Wallace Yacht Services would be happy to provide a free consultation to carry out Maintenance Services to all motor and sailing vessels. <br><br>Call Captain Wallace to make an appointment at (239) 776-2288.</p>
+					
+					<!-- BEGIN .contact-form-1 -->
+<?php
+//if "email" variable is filled out, send email
+  if (isset($_REQUEST['email']))  {
+  
+  //Email information
+  $admin_email = "coganwallace@gmail.com";
+  $email = $_REQUEST['email'];
+  $subject = $_REQUEST['subject'];
+  $comment = $_REQUEST['comment'];
+  
+  //send email
+  mail($admin_email, "$subject", $comment, "From:" . $email);
+  
+  //Email response
+  echo "<b><font color='#4286f4'>Thank you for contacting us!</font></b>";
+  }
+  
+  //if "email" variable is not filled out, display the form
+  else  {
+?>
+
+ <form method="post">
+  Email: <br><br><input name="email" type="text" /><br>
+  Subject: <br><br><input name="subject" type="text" /><br>
+  Message:<br><br>
+  <textarea name="comment" rows="10" cols="40"></textarea>
+  <input class="button1" type="submit" value="Submit Message" />
+  </form>
+  
+<?php
+  }
+?>
+
+					<!-- END .contact-form-1 -->
+					</form>
+					
+				</div>
+			
+				<div class="qns-one-half qns-last">
+					
+					<!-- BEGIN #google-map -->
+
+
+					<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d916854.09921331!2d-82.2505588!3d26.1491012!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88dae19b73c2d7ad%3A0x673f5318d72c9aaa!2sNaples%2C+FL!5e0!3m2!1sen!2sus!4v1501958459015" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+					<br><br>
+					<h4>Contact Details</h4>
+					<div class="title-block5"></div>
+					<div class="widget not-widget">
+						<ul class="contact-details-widget">
+							<li class="cdw-time clearfix">Mon - Sun 8:00 am - 6:00 pm</li>
+							<li class="cdw-phone clearfix">(239) 776-2288</li>
+						</ul>
+					</div>
+					
+					<h4>Social Media</h4>
+					<div class="title-block5"></div>
+					
+					<ul class="social-links clearfix">
+						<li><a href="https://www.facebook.com/pages/Wallace-Yacht-Service/964976123542991" target="_blank"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="https://www.instagram.com/wallaceyachtservices/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+					</ul>
+					
+				</div>
+				
+			<!-- END .main-content -->
+			</section>
+			
+		<!-- END .content-wrapper -->
+		</div>
+		
+	<!-- BEGIN .full-width-message-wrapper -->
+		<section class="full-width-message-wrapper">
+
+			<!-- BEGIN .full-width-message-inner -->
+			<div class="full-width-message-inner">
+
+				<h3><font size="6">Wallace Yacht Services: Quality Yacht Care</font><br><br><br><br></h3>
+
+				<!--<div class="call-to-action-button-wrapper clearfix">
+					<a href="yacht-charter-3-col.html" class="button-view-yachts"><i class="fa fa-arrow-circle-right"></i>View Yachts</a>
+					<a href="contact.php" class="button-get-in-touch"><i class="fa fa-envelope"></i>Get In Touch</a>
+				</div>-->
+
+				<div class="clearboth"></div>
+
+			<!-- END .full-width-message-inner -->
+			</div>
+
+		<!-- END .full-width-message-wrapper -->
+		</section>
+
+	
+	
+		<!-- BEGIN .footer -->
+		<footer class="footer">
+
+			<!-- BEGIN .footer-inner -->
+			<div class="footer-inner clearfix">
+
+				<!-- BEGIN .one-fourth -->
+				<div class="one-fourth">
+
+					<h5>Wallace Yacht Services</h5>
+					<div class="title-block6"></div>
+					<p>Wallace Yacht Services was founded in 1986 to provide high quality marine services to boat owners in Southwest Florida, ensuring a sense of confidence and comfort. </p>
+
+				<!-- END .one-fourth -->
+				</div>
+
+				<!-- BEGIN .one-fourth -->
+				<div class="one-fourth">
+
+					<h5>Mailing List</h5>
+					<div class="title-block6"></div>
+					<p>Subscribe to our mailing list for news, updates, exclusive discounts and offers.</p>
+
+					<!-- BEGIN .newsletter-form -->
+					<form action="#" class="newsletter-form" method="post">
+
+						<input type="text" name="your-name" value="" />
+						<button type="submit">
+			 				<i class="fa fa-envelope"></i> Subscribe
+						</button>
+
+					<!-- END .newsletter-form -->
+					</form>
+
+				<!-- END .one-fourth -->
+				</div>
+
+				<!-- BEGIN .one-fourth -->
+				<div class="one-fourth">
+
+					<h5>Useful Links</h5>
+					<div class="title-block6"></div>
+					<ul>
+						<li><a href="http://Www.marinemax.com">MarineMax</a></li>
+						<li><a href="http://www.meguiars.com ">Meguiars</a></li>
+
+					</ul>
+
+				<!-- END .one-fourth -->
+				</div>
+
+				<!-- BEGIN .one-fourth -->
+				<div class="one-fourth">
+
+					<h5>Contact Info</h5>
+					<div class="title-block6"></div>
+					<ul class="contact-widget">
+						<li class="cw-phone">(239) 776-2288<span>Mon - Sun: 8:00 am - 6:00 pm</span></li>
+					</ul>
+
+				<!-- END .one-fourth -->
+				</div>
+
+			<!-- END .footer-inner -->
+			</div>
+
+			<!-- BEGIN .footer-bottom -->
+			<div class="footer-bottom">
+
+				<div class="footer-bottom-inner clearfix">
+
+					<p class="footer-message">&copy; 2017 Wallace Yacht Services. All Rights Reserved | Powered by <a href="http:www.novusforum.net">NovusForum.net</a></p>
+
+					<div class="footer-social-icons-wrapper">
+						<a href="https://www.facebook.com/pages/Wallace-Yacht-Service/964976123542991"><i class="fa fa-facebook"></i></a>
+						<a href="https://www.instagram.com/wallaceyachtservices/"><i class="fa fa-instagram"></i></a>					
+					</div>
+
+				</div>
+
+			<!-- END .footer-bottom -->
+			</div>
+
+		<!-- END .footer -->	
+		</footer>
+	
+	<!-- END .outer-wrapper -->
+	</div>
+	
+	<!-- JavaScript -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/jquery-ui.min.js"></script>
+	<script type="text/javascript" src="rs-plugin/js/jquery.themepunch.tools.min.js?rev=5.0"></script>
+	<script type="text/javascript" src="rs-plugin/js/jquery.themepunch.revolution.min.js?rev=5.0"></script>
+	<script type="text/javascript" src="js/owl.carousel.min.js"></script>
+	<script type="text/javascript" src="js/jquery.prettyPhoto.js"></script>
+	
+	<!-- Only required for local server -->
+	<script type="text/javascript" src="rs-plugin/js/extensions/revolution.extension.video.min.js"></script>
+	<script type="text/javascript" src="rs-plugin/js/extensions/revolution.extension.slideanims.min.js"></script>
+	<script type="text/javascript" src="rs-plugin/js/extensions/revolution.extension.layeranimation.min.js"></script>
+	<script type="text/javascript" src="rs-plugin/js/extensions/revolution.extension.navigation.min.js"></script>
+	<!-- Only required for local server -->
+	
+	<script type="text/javascript" src="js/scripts.js"></script>
+	
+<!-- END body -->
+</body>
+</html>
